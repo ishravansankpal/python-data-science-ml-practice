3faea9ae41d763b6403fea9ad8185c921cf1df2a
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-22 10:53:51 +0530

    Ques3.py

--- Ques3.py
+++ Ques3.py
@@ -1,3 +1,16 @@
+#  Objective
+# Analyze the House Price Prediction dataset using descriptive statistics and data visualization techniques in Python.
+
+# Tasks Performed
+# - Computed standard deviation for each feature
+# - Computed variance for each feature
+# - Calculated percentiles using separate Python commands
+# - Created histograms for all numerical features to visualize their distributions
+
+# Dataset
+# - House Price Prediction dataset
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -18,26 +31,3 @@ print(df.quantile([0.25,0.50,0.75]))
 # Histogram for numeric columns
 df.hist(figsize=(12,12))
 plt.show()
-
-
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# df = pd.read_csv('House Data.csv')
-
-# print(df.head())
-
-# print(df.dtypes)
-
-# new_df = df.select_dtypes(include = 'number') 
-
-# print(new_df.dtypes)
-
-# print(new_df.std())
-
-# print(new_df.var())
-
-# print(new_df.quantile([0.25,0.50,0.75]))
-
-# df.hist(figsize=(12,12))
-# plt.show()
