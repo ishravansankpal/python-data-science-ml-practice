1cc755f156f0c2ea3a052f9e774448fa832eb801
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-24 17:06:05 +0530

    Add files via upload

---
+++ Ques5.py
@@ -0,0 +1,16 @@
+# import pandas as pd
+# from sklearn.tree import DecisionTreeClassifier
+
+# df = pd.read_csv("Lipstick.csv")
+
+# X = pd.get_dummies(df[['Age','Income','Gender','Ms']])
+# y = df['Buys']
+
+# model = DecisionTreeClassifier()
+# model.fit(X, y)
+
+# test = pd.DataFrame([["<21","Low","Female","Married"]], columns=['Age','Income','Gender','Ms'])
+# test = pd.get_dummies(test).reindex(columns=X.columns, fill_value=0)
+
+# print("Prediction: ", model.predict(test)[0])
+
