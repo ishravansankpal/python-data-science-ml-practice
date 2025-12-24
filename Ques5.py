f029c833ea05dc7c790db70d09264ff78823e20b
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-24 17:06:29 +0530

    Update Ques5.py

--- Ques5.py
+++ Ques5.py
@@ -1,16 +1,17 @@
-# import pandas as pd
-# from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
-# df = pd.read_csv("Lipstick.csv")
+df = pd.read_csv("Lipstick.csv")
 
-# X = pd.get_dummies(df[['Age','Income','Gender','Ms']])
-# y = df['Buys']
+X = pd.get_dummies(df[['Age','Income','Gender','Ms']])
+y = df['Buys']
 
-# model = DecisionTreeClassifier()
-# model.fit(X, y)
+model = DecisionTreeClassifier()
+model.fit(X, y)
 
-# test = pd.DataFrame([["<21","Low","Female","Married"]], columns=['Age','Income','Gender','Ms'])
-# test = pd.get_dummies(test).reindex(columns=X.columns, fill_value=0)
+test = pd.DataFrame([["<21","Low","Female","Married"]], columns=['Age','Income','Gender','Ms'])
+test = pd.get_dummies(test).reindex(columns=X.columns, fill_value=0)
 
-# print("Prediction: ", model.predict(test)[0])
+print("Prediction: ", model.predict(test)[0])
+
 
