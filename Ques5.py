804c3a1b638fd7435991b4cbe41e7dc4e397be9d
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-24 17:08:08 +0530

    Update Ques5.py

--- Ques5.py
+++ Ques5.py
@@ -1,3 +1,23 @@
+#  Objective
+# Predict customer response to a lipstick offer using a Decision Tree model
+# implemented with machine learning libraries.
+
+#  Tasks Performed
+# - Trained a Decision Tree classifier using the given dataset
+# - Used library-based implementation (scikit-learn)
+# - Predicted the outcome for the following test data:
+
+#   Age < 21  
+#   Income = Low  
+#   Gender = Female  
+#   Marital Status = Married  
+
+# Implementation
+# - Decision Tree implemented using Python ML libraries
+# - Model trained on previous dataset and used for prediction
+
+
+
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
@@ -13,5 +33,6 @@ test = pd.DataFrame([["<21","Low","Female","Married"]], columns=['Age','Income',
 test = pd.get_dummies(test).reindex(columns=X.columns, fill_value=0)
 
 print("Prediction: ", model.predict(test)[0])
+
 
 
