8e7a1a929912439d7ae86a3dc2c541ee987baed2
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-28 17:08:11 +0530

    Update Ques12.py

--- Ques12.py
+++ Ques12.py
@@ -1,21 +1,14 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-
-# # 1. Load Data
-# df = pd.read_csv('IRIS.csv')
-
-# # 2. Create box plot for each feature
-# plt.figure(figsize=(10, 8))
-# df.boxplot()
-# plt.title("Box Plots of Iris Dataset Features")
-# plt.show()
-
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# 1. Load Data
 df = pd.read_csv('IRIS.csv')
 
-plt.figure(figsize=(10,10))
+# 2. Create box plot for each feature
+plt.figure(figsize=(10, 8))
 df.boxplot()
+plt.title("Box Plots of Iris Dataset Features")
 plt.show()
+
+
