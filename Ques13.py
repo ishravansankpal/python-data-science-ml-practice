0189e1eaa90c22861e4a679cff14eaab145c812a
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2025-12-29 19:27:55 +0530

    Update Ques13.py

--- Ques13.py
+++ Ques13.py
@@ -1,28 +1,39 @@
-# import pandas as pd
+# Objective
+# Analyze the COVID-19 vaccination data in India using the `covid_vaccine_statewise.csv` dataset.
 
-# # Load dataset
-# df = pd.read_csv("Covid Vaccine Statewise.csv")
+# Tasks Performed
+# - Described the overall structure and attributes of the dataset
+# - Analyzed state-wise vaccination data
+# - Computed the total number of people vaccinated with:
+#   - First dose (state-wise)
+#   - Second dose (state-wise)
 
-# # a. Describe the dataset
-# print(df.describe(include='all'))
+# Dataset
+# - `covid_vaccine_statewise.csv`
+# - Contains state-wise COVID-19 vaccination details in India
 
-# # b. Number of persons state-wise vaccinated with FIRST dose
-# first_dose = df.groupby("State")["First Dose Administered"].sum()
-# print(first_dose)
+# Key Concepts Used
+# - Data exploration
+# - Data aggregation
+# - Group-by operations
+# - Basic data analytics using Python
 
-# # c. Number of persons state-wise vaccinated with SECOND dose
-# second_dose = df.groupby("State")["Second Dose Administered"].sum()
-# print(second_dose)
+# Tools & Libraries
+# - Python
+# - Pandas
 
+import pandas as pd
 
-import pandas as pd 
+# Load dataset
+df = pd.read_csv("Covid Vaccine Statewise.csv")
 
-df = pd.read_csv('Covid Vaccine Statewise.csv')
+# a. Describe the dataset
+print(df.describe(include='all'))
 
-print(df.describe())
-
-first_dose = df.groupby('State')['First Dose Administered'].sum()
+# b. Number of persons state-wise vaccinated with FIRST dose
+first_dose = df.groupby("State")["First Dose Administered"].sum()
 print(first_dose)
 
-second_dose = df.groupby('State')['Second Dose Administered'].sum()
+# c. Number of persons state-wise vaccinated with SECOND dose
+second_dose = df.groupby("State")["Second Dose Administered"].sum()
 print(second_dose)
