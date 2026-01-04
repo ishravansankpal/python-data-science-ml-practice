00fbddda7ff7cec126fcf2ea16938fe3d1a12b62
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2026-01-04 16:20:03 +0530

    Update Ques21.py

--- Ques21.py
+++ Ques21.py
@@ -1,37 +1,22 @@
-# import pandas as pd
-# from sklearn.cluster import KMeans
-
-# # Load IRIS dataset
-# df = pd.read_csv("IRIS.csv")
-
-# # Use only numeric columns (sepal/petal measurements)
-# X = df.select_dtypes(include='number')
-
-# # Run K-Means: 4 clusters, random init, 10 iterations
-# kmeans = KMeans(n_clusters=4,
-#                 init='random',     # random initial centroids
-#                 max_iter=10,       # at least 10 iterations
-#                 n_init=1,          # run only once (as required)
-#                 random_state=42)
-
-# # Fit model
-# kmeans.fit(X)
-
-# # Print final cluster means (centroids)
-# print("Final Cluster Means (Centroids):")
-# print(kmeans.cluster_centers_)
-
-
-import pandas as pd 
-
+import pandas as pd
 from sklearn.cluster import KMeans
 
-df = pd.read_csv('IRIS.csv')
+# Load IRIS dataset
+df = pd.read_csv("IRIS.csv")
 
-X = df.select_dtypes(include = 'number')
+# Use only numeric columns (sepal/petal measurements)
+X = df.select_dtypes(include='number')
 
-KMeans = KMeans(n_clusters=4, init='random',max_iter=10, n_init=1, random_state=1)
+# Run K-Means: 4 clusters, random init, 10 iterations
+kmeans = KMeans(n_clusters=4,
+                init='random',     # random initial centroids
+                max_iter=10,       # at least 10 iterations
+                n_init=1,          # run only once (as required)
+                random_state=42)
 
-KMeans.fit(X)
+# Fit model
+kmeans.fit(X)
 
-print(KMeans.cluster_centers_)
+# Print final cluster means (centroids)
+print("Final Cluster Means (Centroids):")
+print(kmeans.cluster_centers_)
