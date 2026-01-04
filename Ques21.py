5cb2b0c6055f921eadd3631ff2a3dd39af17a5b5
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2026-01-04 16:21:05 +0530

    Update Ques21.py

--- Ques21.py
+++ Ques21.py
@@ -1,3 +1,9 @@
+# 21. Write a program to cluster a set of points using K-means for IRIS dataset. Consider, K=4, clusters. Consider Euclidean distance as the 
+# distance measure. Randomly initialize a cluster mean as one of the data
+# points. Iterate at least for 10 iterations. After iterations are over, print the
+# final cluster means for each of the clusters.
+
+             
 import pandas as pd
 from sklearn.cluster import KMeans
 
@@ -20,3 +26,4 @@ kmeans.fit(X)
 # Print final cluster means (centroids)
 print("Final Cluster Means (Centroids):")
 print(kmeans.cluster_centers_)
+
