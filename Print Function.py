2db96fef86e42c4f94d97fa43e145587a6c022d4
Shravan Sankpal <shravan.s.sankpal@gmail.com>
2026-01-31 19:35:15 +0530

    Print Function.py

--- Print Function.py
+++ Print Function.py
@@ -1,1 +1,10 @@
+# The included code stub will read an integer, n , from STDIN.
+# Without using any string methods, try to print the following: 123...n
 
+def diaplay(n):
+    for i in range(n):
+        print(i+1, end="")
+        
+if __name__ == '__main__':
+    n = int(input())
+    diaplay(n)
